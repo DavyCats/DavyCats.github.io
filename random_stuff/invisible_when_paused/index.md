742d3dd0efe6012a6c5facf1f870b57eb7dd3d76
DavyCats <davycats.dc@gmail.com>
2025-06-13 16:22:50 +0200

    cleanup hidden when paused, allow user selected image source

--- random_stuff/invisible_when_paused/index.md
+++ random_stuff/invisible_when_paused/index.md
@@ -3,6 +3,9 @@ layout: page
 ---
 
 <canvas width="1024" height="512" id="shader_canvas"></canvas>
+
 <button id="pause_button" onclick="pause()">Pause</button>
-<button id="alt" onclick="switch_image()">switch</button>
+
+Load a file (image or video): <input id="image_src" type="file" onchange="switch_image()">, or <button id="reset" onclick="load_image('./text.png')">reload the default image</button>.
+
 <script src="./shader.js"></script>
--- random_stuff/invisible_when_paused/shader.js
+++ random_stuff/invisible_when_paused/shader.js
@@ -1,6 +1,3 @@
-/* THIS IS A MESS and needs a bunch of cleanup/refactoring....
- */
-
 // Vertex shader source code
 const vertexShaderSource = `
     attribute vec4 aVertexPosition;
@@ -20,19 +17,51 @@ const fragmentShaderSource = `
         vec4 ones = vec4(1., 1., 1., 1.);
         vec2 uv = gl_FragCoord.xy / uResolution.xy;
         vec4 this_pixel = texture2D(uImageTexture, uv);
+        vec4 this_noise = texture2D(uNoiseTexture, uv);
+
+        //float r = this_pixel.x < 0.5 ? abs(this_noise.x - 1.): this_noise.x;
+        //float g = this_pixel.y < 0.5 ? abs(this_noise.y - 1.): this_noise.y;
+        //float b = this_pixel.z < 0.5 ? abs(this_noise.z - 1.): this_noise.z;
+        //gl_FragColor = vec4(r, g, b, 1.);
+
         float value = this_pixel.x + this_pixel.y + this_pixel.z;
         if (value < 1.5) {
-            gl_FragColor = vec4(abs(texture2D(uNoiseTexture, uv).xyz - ones.xyz), 1.);
+            gl_FragColor = vec4(abs(this_noise.xyz - ones.xyz), 1.);
         } else {
-            gl_FragColor = texture2D(uNoiseTexture, uv);
+            gl_FragColor = this_noise;
         }
     }
 `;
 
 var PAUSE = false;
-var PLAYING_VIDEO = false;
-var video_playing = false
-var video_timeupdate = false;
+var VIDEO = false;
+var VIDEO_IS_PLAYING = false
+var VIDEO_HAS_HAD_TIMEUPDATE = false;
+var SOURCE_ELEMENT;
+const CANVAS = document.getElementById("shader_canvas");
+
+function saltPepperNoise(width, height){
+    var out = [];
+    for (var i = 0; i < width*height; i++){
+        const val = Math.random() > 0.5 ? 255: 0;
+        out.push(val);
+        out.push(val);
+        out.push(val);
+        out.push(255);
+    }
+    return out;
+}
+
+function rgbNoise(width, height){
+    var out = [];
+    for (var i = 0; i < width*height; i++){
+        out.push(Math.random() > 0.5 ? 255: 0);
+        out.push(Math.random() > 0.5 ? 255: 0);
+        out.push(Math.random() > 0.5 ? 255: 0);
+        out.push(255);
+    }
+    return out;
+}
 
 function pause() {
     PAUSE = ! PAUSE;
@@ -42,62 +71,96 @@ function pause() {
 }
 
 function stop() {
-    PAUSE = true
+    PAUSE = true;
 }
 
 function start() {
-    PAUSE = false
+    PAUSE = false;
     requestAnimationFrame(render);
 }
 
+function setup_textures() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, ImageTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, SOURCE_ELEMENT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    // make noise
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, noiseTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, CANVAS.width, CANVAS.height, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+        new Uint8Array(saltPepperNoise(CANVAS.width, CANVAS.height)));
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+}
+
 function load_image(url) {
-    // load image
     stop();
-    image.src = url;
-    PLAYING_VIDEO = false
+    SOURCE_ELEMENT = document.createElement("img");
+    SOURCE_ELEMENT.addEventListener('load', image_ready);
+    SOURCE_ELEMENT.src = url;
+    VIDEO = false;
 }
 
 // video
 function load_video(url) {
     stop();
-    video_timeupdate = false;
-    video_playing = false;
-    video.src = url;
-    video.volume = 0;
-    video.loop = true;
-    video.play();
+    SOURCE_ELEMENT = document.createElement("video");
+    VIDEO_HAS_HAD_TIMEUPDATE = false;
+    VIDEO_IS_PLAYING = false;
+    SOURCE_ELEMENT.volume = 0;
+    SOURCE_ELEMENT.loop = true;
+    // checking both playing and timeupdate to ensure that data is available before rendering
+    SOURCE_ELEMENT.addEventListener('playing', function() {
+        VIDEO_IS_PLAYING = true;
+        video_ready();
+    });
+    SOURCE_ELEMENT.addEventListener('timeupdate', function() {
+        if (! VIDEO_HAS_HAD_TIMEUPDATE) {
+            VIDEO_HAS_HAD_TIMEUPDATE = true;
+            video_ready();
+        }
+    });
+    SOURCE_ELEMENT.src = url;
+    SOURCE_ELEMENT.play();
+}
+
+function image_ready() {
+    CANVAS.width = SOURCE_ELEMENT.width;
+    CANVAS.height = SOURCE_ELEMENT.height;
+    setup_textures();
+    start();
 }
 
 function video_ready() {
-    if (video_playing && video_timeupdate) {
+    if (VIDEO_IS_PLAYING && VIDEO_HAS_HAD_TIMEUPDATE) {
         // This is pretty much the as with an image, except that width/height use different variables
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, ImageTexture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-        // make noise
-        gl.activeTexture(gl.TEXTURE1);
-        gl.bindTexture(gl.TEXTURE_2D, noiseTexture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, canvas.width, canvas.height, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-            new Uint8Array(saltPepperNoise(canvas.width, canvas.height)));
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-        PLAYING_VIDEO = true;
+        CANVAS.width = SOURCE_ELEMENT.videoWidth;
+        CANVAS.height = SOURCE_ELEMENT.videoHeight;
+        setup_textures();
+        VIDEO = true;
         start();
     }
 }
 
 function switch_image() {
-    load_video("Firefox.mp4")
-}
+    const file = document.getElementById("image_src").files[0];
+    console.log(file);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        if (file.type.startsWith("image/")) {
+            load_image(e.target.result);
+        } else if (file.type.startsWith("video/")) {
+            load_video(e.target.result);
+        } else {
+            alert("File type not supported.")
+        }
+    };
+    reader.readAsDataURL(file);
+  }
 
 function compileShader(source, type) {
     const shader = gl.createShader(type);
@@ -112,94 +175,6 @@ function compileShader(source, type) {
 
     return shader;
 }
-
-
-function saltPepperNoise(width, height){
-    var out = [];
-    for (var i = 0; i < width*height; i++){
-        const val = Math.random() > 0.5 ? 255: 0;
-        out.push(val);
-        out.push(val);
-        out.push(val);
-        out.push(255);
-    }
-    return out;
-}
-
-// Setup the canvas and webgl context.
-const canvas = document.getElementById("shader_canvas");
-const gl = canvas.getContext('webgl');
-gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-
-// Make sure webGL is supported.
-if (!gl) {
-    console.error('WebGL not supported, falling back on experimental-webgl');
-    gl = canvas.getContext('experimental-webgl');
-}
-
-if (!gl) {
-    alert('Your browser does not support WebGL');
-}
-
-// Create shaders
-const vertexShader = compileShader(vertexShaderSource, gl.VERTEX_SHADER);
-const fragmentShader = compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
-
-// Create the shader program
-const shaderProgram = gl.createProgram();
-gl.attachShader(shaderProgram, vertexShader);
-gl.attachShader(shaderProgram, fragmentShader);
-gl.linkProgram(shaderProgram);
-
-if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-}
-
-const ImageTexture = gl.createTexture();
-const noiseTexture = gl.createTexture();
-var image = new Image();
-image.addEventListener('load', function() {
-    canvas.width = image.width;
-    canvas.height = image.height;
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, ImageTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-    // make noise
-    gl.activeTexture(gl.TEXTURE1);
-    gl.bindTexture(gl.TEXTURE_2D, noiseTexture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, canvas.width, canvas.height, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-        new Uint8Array(saltPepperNoise(canvas.width, canvas.height)));
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    start();
-});
-
-// Setup a video element, checking both playing and timeupdate to ensure that data is available before rendering
-var video = document.createElement('video');
-video.addEventListener('playing', function() {
-    video_playing = true;
-    video_ready();
-})
-video.addEventListener('timeupdate', function() {
-    if (! video_timeupdate) {
-        video_timeupdate = true;
-        video_ready();
-    }
-})
-
-// load default image
-load_image("./text.png")
-
-// fps control stuff
-var lastFrameDrawnAt = window.performance.now();
-var fps = 60;
-var frameInterval = 1000/fps;
 
 function render(time) {
     var elapsed = time - lastFrameDrawnAt;
@@ -230,7 +205,7 @@ function render(time) {
         var imageTextureLocation = gl.getUniformLocation(shaderProgram, "uImageTexture");
         var noiseTextureLocation = gl.getUniformLocation(shaderProgram, "uNoiseTexture");
 
-        gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+        gl.uniform2f(resolutionUniformLocation, CANVAS.width, CANVAS.height);
         gl.uniform1i(imageTextureLocation, 0);
         gl.uniform1i(noiseTextureLocation, 1);
 
@@ -238,18 +213,56 @@ function render(time) {
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-        if (PLAYING_VIDEO) {
+        if (VIDEO) {
             gl.activeTexture(gl.TEXTURE0);
-            //gl.bindTexture(gl.TEXTURE_2D, ImageTexture);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, src);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, SOURCE_ELEMENT);
         }
 
         // Save as previous frame
         gl.activeTexture(gl.TEXTURE1);
-        gl.copyTexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 0, 0, canvas.width, canvas.height, 0);
+        gl.copyTexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 0, 0, CANVAS.width, CANVAS.height, 0);
     }
 
     if (!PAUSE) {
         requestAnimationFrame(render);
     }
 }
+
+// Setup the canvas and webgl context.
+const gl = CANVAS.getContext('webgl');
+gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+// Make sure webGL is supported.
+if (!gl) {
+    console.error('WebGL not supported, falling back on experimental-webgl');
+    gl = CANVAS.getContext('experimental-webgl');
+}
+
+if (!gl) {
+    alert('Your browser does not support WebGL');
+}
+
+// Create shaders
+const vertexShader = compileShader(vertexShaderSource, gl.VERTEX_SHADER);
+const fragmentShader = compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
+
+// Create the shader program
+const shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertexShader);
+gl.attachShader(shaderProgram, fragmentShader);
+gl.linkProgram(shaderProgram);
+
+if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+}
+
+const ImageTexture = gl.createTexture();
+const noiseTexture = gl.createTexture();
+
+// fps control stuff
+var lastFrameDrawnAt = window.performance.now();
+var fps = 60;
+var frameInterval = 1000/fps;
+
+// load default image
+load_image("./text.png")
